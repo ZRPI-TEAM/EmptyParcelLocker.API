519411d99a00f0b45ce6167d90914d3be58cbcf9
Jan Urbaś <janurbas39@gmail.com>
2022-08-22 12:30:37 +0200

    Create ParcelLockerDbContext.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using EmptyParcelLocker.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ParcelLockerDbContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
